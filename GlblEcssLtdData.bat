e507f74ec122763c80e2749b6a302bea1623eeaf
Mike Martin <mikemartin@ifb.co.uk>
2024-04-12 07:29:03 +0100

    Update GlblEcssLtdData.bat

--- GlblEcssLtdData.bat
+++ GlblEcssLtdData.bat
@@ -9,12 +9,3 @@
 @chdir /D %setup_dir%
 start cmd.exe /k "%python_exe% -W ignore %source_dir%GlblEcsseHwsdGUI.py"
 @chdir /D %initial_working_dir%
-
-
-rem 
-@set source_dir=E:\AbUniv\
-@set PYTHONPATH=%source_dir%EnvModelModules;%source_dir%GlblEcosseModulesLtd
-@set initial_working_dir=%cd%
-@chdir /D %source_dir%GlobalEcosseSuite\setup\ltd_data
-start cmd.exe /k "E:\Python38\python.exe -W ignore %source_dir%GlblEcosseVer2\GlblEcsseHwsdGUI.py"
-@chdir /D %initial_working_dir%
